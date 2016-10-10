26c4de328a206d9dc1517cb7ab75d325f3c3503e
jlvelazquez <jlvelazquez@gmail.com>
2016-10-10 15:00:20 +0200

    delete method findInArray (useless?)

--- src/shell/argos/argos-json-utils.js
+++ src/shell/argos/argos-json-utils.js
@@ -14,18 +14,6 @@ const jsonUtils = (function () {
     return JSON.parse(JSON.stringify(obToCopy));
   };
 
-  module.findInArray = (_array_, valueToFind)=> {
-    let i   = 0;
-    let len = _array.length;
-
-    for (; i < len; i++) {
-      if ( valueToFind === _array_[i] ) {
-        return _array_[i];
-      }
-    }
-    return false;
-  };
-
   module.findInObject = (_array_, key, value) => _array_.find(item => item[key] === value);
 
   return {
