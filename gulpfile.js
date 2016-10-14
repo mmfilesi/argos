aa74d0b78092457d648113681944e48320951376
Marcos Mendez Filesis <mmendez@vectoritcgroup.com>
2016-10-14 11:02:54 +0200

    gulp in job

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,4 @@
-/* todos:
-  sass
-  el imagemin no chufla
-*/
+'use strict';
 
 const gulp              = require('gulp');
 const babel             = require('gulp-babel');
@@ -9,7 +6,7 @@ const browserSync       = require('browser-sync').create();
 const concat            = require('gulp-concat');
 const uglify            = require('gulp-uglify');
 const sourcemaps        = require('gulp-sourcemaps');
-// const imagemin          = require('gulp-imagemin');
+//const imagemin          = require('gulp-imagemin');
 const cleanCSS          = require('gulp-clean-css');
 const clean             = require('gulp-clean');
 const gutil             = require('gulp-util');
@@ -42,29 +39,23 @@ const optionsServer = {
   Paths
 ==================================== */
 
-'use strict';
-
 const baseSrc     = './src/';
 const baseDist    = './dist/';
 const pathsSrc    = {};
 const pathsDist   = {};
 
-pathsSrc.theme      = baseSrc + 'shell/theme/';
-pathsDist.theme     = baseDist + 'shell/theme/';
 
-pathsSrc.core       = baseSrc + 'shell/argos/**/argos*.js';
-pathsDist.core      = baseDist + 'shell/argos/';
+pathsSrc.shell       = baseSrc + 'shell/';
+pathsDist.shell      = baseDist + 'shell/';
 
-pathsSrc.index      = baseSrc + 'index.html';
-pathsDist.index     = baseDist;
+pathsSrc.theme      = pathsSrc.shell + 'theme/';
+pathsDist.theme     = pathsDist.shell + 'theme/';
+
+pathsSrc.core       = pathsSrc.shell + 'argos/**/argos*.js';
+pathsDist.core      = pathsDist.shell + 'argos/';
 
 pathsSrc.views       = baseSrc + 'shell/theme/views/**/*';
 pathsDist.views      = baseDist + 'shell/theme/views/';
-
-pathsSrc.configFiles   = [
-  './src/shell/theme/*.json',
-  './src/shell/theme/*.js'
-];
 
 pathsSrc.rootFiles   = [
   './src/favicon.ico',
@@ -73,21 +64,12 @@ pathsSrc.rootFiles   = [
   './src/sw.js'
 ];
 
-pathsSrc.images     = baseSrc + 'shell/styles/img/*';
-pathsDist.images    = baseDist + 'shell/styles/img/';
-
-pathsSrc.styles     = baseSrc + 'shell/theme/styles/css/**/*';
-pathsDist.styles    = baseDist + 'shell/theme/styles/';
-
-pathsSrc.mocks      = baseSrc + 'mocks/**/*.json';
-pathsDist.mocks     = baseDist + 'mocks/';
-
+/* Añadir aquí todas las librerías vendor estrictamente necesarias
+para que funcione la aplicación  nada más cargar (el resto, por lazy load en app.js) */
 pathsSrc.vendor     = [
-  './src/shell/vendor/q/q.js',
-  './src/shell/vendor/d3/d3.min.js'
+  pathsSrc.shell + 'vendor/q/q.js',
+  pathsSrc.shell + 'vendor/handlebars/handlebars.js'
 ];
-
-pathsDist.vendor    = baseDist + 'shell/vendor';
 
 /* ====================================
   Tasks
@@ -112,13 +94,29 @@ gulp.task('doc', (done)=> {
   task - builder
 ======================== */
 
-  gulp.task('buildRootFiles', (done)=> {
-      gulp.src(pathsSrc.rootFiles)
-      .pipe(gulp.dest(baseDist));
-      done();
-  });
+/* Separamos los task del builder en tres grupos:
+- los archivos "estáticos" que en teoría apenas cambian a lo largo del desarrollo (ie, vendor)
+- los estilos
+- los js
+De esta manera, se puede jugar en el watcher para aliviar el proceso */
 
-  gulp.task('buildCore', (done)=> {
+gulp.task('buildStatics', (done)=> {
+
+  /* librerías vendor :: esto al final lo hacemos con el useref */
+  // gulp.src( pathsSrc.vendor )
+  //   .pipe( concat('all-libraries.js', optionsConcat) )
+  //   .pipe(gulp.dest(baseDist + 'shell/vendor'));
+
+    if ( ENVIROMENT !== 'production' ) {
+      gulp.src(baseSrc + 'mocks/**/*.json')
+        .pipe(gulp.dest(baseDist + 'mocks/'));
+    }
+
+    /* configuración general */
+    gulp.src(pathsSrc.rootFiles)
+      .pipe(gulp.dest(baseDist));
+
+    /* core */
     gulp.src(pathsSrc.core)
       .pipe(sourcemaps.init())
       .pipe(babel({
@@ -128,87 +126,80 @@ gulp.task('doc', (done)=> {
       .pipe( ENVIROMENT === 'production' ? uglify(optionsUglify) :  gutil.noop() )
       .pipe(sourcemaps.write('./'))
       .pipe( gulp.dest(pathsDist.core) );
-      done()
-  });
 
-  gulp.task('buildIndex', (done)=> {
-      gulp.src(pathsSrc.index)
-      .pipe(useref())
-      .pipe(gulp.dest(pathsDist.index));
-      done();
-  });
-
-  gulp.task('buildConfigFiles', (done)=> {
-    gulp.src(pathsSrc.configFiles)
+    /* Archivos de configuración del tema */
+    gulp.src(pathsSrc.theme + '*.json')
       .pipe(gulp.dest(pathsDist.theme));
-     done();
-  });
 
-  gulp.task('buildViews', (done)=> {
-    gulp.src(pathsSrc.views  + '**/*.js' )
+  done();
+});
+
+gulp.task('buildTheme', (done)=> {
+  let themeSources = [baseSrc + 'shell/theme/*.js', baseSrc + 'shell/theme/models/**/*.js'];
+
+    gulp.src(themeSources)
+     .pipe(sourcemaps.init())
       .pipe(babel({
             presets: ['es2015']
         }))
+      .pipe( concat('app.min.js', optionsConcat) )
+      .pipe( ENVIROMENT === 'production' ? uglify(optionsUglify) :  gutil.noop() )
+      .pipe(sourcemaps.write('./'))
+      .pipe(gulp.dest(pathsDist.theme));
+
+    gulp.src(pathsSrc.views  + '**/*.html' )
       .pipe(gulp.dest(pathsDist.views));
-     done();
-  });
 
-  gulp.task('buildMocks', (done)=> {
-    if ( ENVIROMENT !== 'production' ) {
-      gulp.src('./src/mocks/**/*.json')
-        .pipe(gulp.dest('./dist/mocks/'));
-    }
-     done();
-  });
+    gulp.src(pathsSrc.theme + 'langs/**/*.json')
+      .pipe(gulp.dest(pathsDist.theme + 'langs/'));
 
-  gulp.task('buildStatics', (done)=> {
-    gulp.src('./src/shell/theme/langs/**/*.json')
-      .pipe(gulp.dest('./dist/shell/theme/langs/'));
-     done();
-  });
+  done();
+});
 
   /* De momento va sin sass */
   gulp.task('buildStyles', (done)=> {
-    gulp.src(pathsSrc.styles)
+    gulp.src(pathsSrc.theme + 'styles/css/**/*.css')
       .pipe(concat('styles.css'))
       .pipe( ENVIROMENT === 'develop' ? sourcemaps.init() :  gutil.noop() )
       .pipe(cleanCSS())
       .pipe(concat('styles.min.css'))
       .pipe( ENVIROMENT === 'develop' ? sourcemaps.write() :  gutil.noop() )
-      .pipe(gulp.dest(pathsDist.styles));
-     done();
+      .pipe(gulp.dest(pathsDist.theme + 'styles/'));
+
+      gulp.src(pathsSrc.theme + 'styles/img/**/*.*')
+      .pipe(gulp.dest(pathsDist.theme + 'styles/img'));
+
+      done();
   });
 
-  gulp.task('buildVendor', (done)=> {
-    gulp.src(pathsSrc.vendor)
-      .pipe( concat('all-libraries.js', optionsConcat) )
-      .pipe(gulp.dest(pathsDist.vendor));
-    done();
+  /* El index hay que separarlo, ya que el useref reemplazaría los archivos transpilados
+  por los originales */
+  gulp.task('buildIndex', (done)=> {
+      // gulp.src(baseSrc + 'index.html')
+      // .pipe(useref())
+      // .pipe(gulp.dest(baseDist));
+      // done();
   });
 
 /* ========================
-  #task - builder
+  task - builder
 ======================== */
 
-gulp.task('build', gulp.series(
-  'buildRootFiles',
-  'buildCore',
-  'buildIndex',
-  'buildViews',
-  'buildVendor',
-  'buildConfigFiles',
-  'buildMocks',
-  'buildStyles',
-  'buildStatics',
-  (done)=> {
-    done();
-  }));
-
 gulp.task('clean', (done)=> {
-  gulp.src(baseDist + '/*', {read: false})
+  gulp.src(baseDist + '*', {read: false})
     .pipe(clean());
   done();
 });
+
+gulp.task('build', gulp.series(
+  'clean',
+ // 'buildIndex',
+  'buildStatics',
+  'buildTheme',
+  'buildStyles',
+  (done)=> {
+    done();
+  }));
 
 gulp.task('default', gulp.series('build'), (done)=> {
   gutil.log('Gulp is running!');
